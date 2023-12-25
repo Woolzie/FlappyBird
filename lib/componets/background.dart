3bdc8988784c82ccc3e9987477fd686f5c857301
sana <snana.swag@gmail.com>
2023-12-25 13:51:30 +0530

    omfg the background thing workedddd YAYAYYA

--- lib/componets/background.dart
+++ lib/componets/background.dart
@@ -1,28 +1,39 @@
 
-
-import 'dart:ffi';
-
 import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
+import 'package:flame/parallax.dart';
+import 'package:flappybird/componets/config.dart';
 import 'package:flappybird/game/flappy_bird.dart';
 
 import '../game/assets.dart';
 
 
-class Background extends SpriteComponent with HasGameRef<FlappyBirdGame> {
+class Background extends ParallaxComponent<FlappyBirdGame>{
   Background();
 
 
 
   @override
-  Future<void> onLoad() async {
+  // Future<void> onLoad() async {
+  //   final background = await Flame.images.load(Assets.backgroundDay);
+  //   size = gameRef.size;
+  //   sprite =Sprite(background);
+  // }
+  //   void resize(Size size) {
+  //   super.autoResize;
+  //   }
+  Future<void> onLoad() async{
     final background = await Flame.images.load(Assets.backgroundDay);
-    size = gameRef.size;
-    sprite =Sprite(background);
-  }
-    void resize(Size size) {
-    super.autoResize;
-    }
+    parallax= Parallax([
+      ParallaxLayer(
+        ParallaxImage(background),
+      )
+    ]);
 
+  }
+  void update(double dt){
+    super.update(dt);
+    parallax?.baseVelocity.x =Config.gameSpeed/3;
+  }
 
 }
